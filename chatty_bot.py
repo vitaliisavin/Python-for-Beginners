0739854e9a4970527968da3e7912fc20e44f18f7
vitaliisavin <111984737+vitaliisavin@users.noreply.github.com>
2023-07-05 18:38:24 +0200

    Updateіва chatty_bot.py

--- chatty_bot.py
+++ chatty_bot.py
@@ -1,6 +1,6 @@
 def greet(bot_name, birth_year):
     print('Hello! My name is ' + bot_name + '.')
-    print('I was created in ' + birth_year + '.')
+    print('I was created in ' + birth_year + '.')іваіва
 
 
 def remind_name():
